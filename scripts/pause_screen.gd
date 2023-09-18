e3ab6188b6cb6b0d9e25c10277b1edb17060ff7e
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-09-18 15:39:15 -0400

    Pause screen

---
+++ scripts/pause_screen.gd
@@ -0,0 +1,28 @@
+class_name PauseScreen extends CanvasLayer
+
+
+@onready var layout: Control = $Layout
+@onready var continue_button: TextureButton = %ContinueButton
+@onready var paused := false:
+	set(value):
+		if get_tree().paused == value:
+			return
+
+		paused = value
+		get_tree().paused = paused
+		layout.visible = paused
+		if paused:
+			continue_button.grab_focus()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("select"):
+		paused = not paused
+
+
+func _on_continue_button_pressed() -> void:
+	paused = false
+
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
--- scripts/tutorial_level.gd
+++ scripts/tutorial_level.gd
@@ -27,26 +27,26 @@ func _on_text_box_finished() -> void:
 	match tutorial_index:
 		1:
 			await ship.moved_both_ways
-			await get_tree().create_timer(2.0).timeout
+			await get_tree().create_timer(2.0, false).timeout
 			text_box.print_text("Your galactic garbage truck is at the bottom.",
 					"start", Control.PRESET_TOP_WIDE)
 		2:
 			hook.set_process_input(false)
 			spawner.spawn(preload("res://scenes/trash.tscn")).collected = true
-			await get_tree().create_timer(1.0).timeout
+			await get_tree().create_timer(1.0, false).timeout
 			hook_trigger.enabled = true
 		3:
-			await get_tree().create_timer(1.5).timeout
+			await get_tree().create_timer(1.5, false).timeout
 			text_box.print_text("Beware: Grabbing stuff besides trash is like grabbing a porcupine.")
 		4:
 			astroid = spawner.spawn(preload("res://scenes/obstacle.tscn"))
-			await get_tree().create_timer(1.0).timeout
+			await get_tree().create_timer(1.0, false).timeout
 			text_box.print_text("An astroid! Don't let it hit your garbage truck.")
 		5:
 			text_box.print_text("Press A to shoot it down.")
 		6:
 			await astroid.destroyed
-			await get_tree().create_timer(1.0).timeout
+			await get_tree().create_timer(1.0, false).timeout
 			text_box.print_text("Goal: Collect as much trash as possible!")
 		7:
 			spawner.set_process(true)
